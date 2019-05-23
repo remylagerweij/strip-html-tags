40fcab2e6e49e86e7a2543ed145bdb09f9b59f1f
Bing Steup <bingsteup@gmail.com>
2019-05-23 09:41:15 +0200

    Add escape hatch for when value is not a string

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const stripHtmlTags = (str) => {
-  let content = str;
-  if (content === null || content === '') return false;
-  content = content.toString();
-  return content.replace(/<[^>]*>/g, '');
+  if (typeof str !== 'string') {
+    return '';
+  }
+
+  return str.toString().replace(/<[^>]*>/g, '');
 };
 
 export default stripHtmlTags;
